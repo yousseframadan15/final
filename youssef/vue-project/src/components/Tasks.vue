c8f9ba1c806857cfd650735f23bd9b203488df5e
Youssef ramadan <yramadan395@gmail.com>
2023-08-15 16:17:48 +0300

    vue

---
+++ youssef/vue-project/src/components/Tasks.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <div v-for="task in tasks" :key="task.id">
+      <h3>
+        {{ task.text }}
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+};
+</script>
