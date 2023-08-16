a77c7b29890b114b26f3d53d766e98a93ad7a646
Youssef ramadan <yramadan395@gmail.com>
2023-08-16 14:52:53 +0300

    task

---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      console.log(this.$route)
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ vue-project/vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: '8080',
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000/",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+
+      }
+    }
+  }
+})
